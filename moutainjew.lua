0eed2b9eb45afd4d6ea1f3af9b50318646d0dd03
xurelmember <tjwilliams987@icloud.com>
2025-08-27 07:04:07 +0100

    Update moutainjew.lua

--- moutainjew.lua
+++ moutainjew.lua
@@ -33,7 +33,7 @@ local Window = Rayfield:CreateWindow({
       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Dewisout"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Key = {"Haroldismonkey"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
 
